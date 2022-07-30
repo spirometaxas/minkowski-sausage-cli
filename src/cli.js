6e7c10a6b6d20d26bbbc1242558f41abd9978f5e
Spiro Metaxas <spirometaxas@gmail.com>
2022-07-30 12:49:55 -0400

    Updates

--- src/cli.js
+++ src/cli.js
@@ -2,15 +2,50 @@
 const minkowski_sausage = require('./index.js');
 
 const printUsage = function() {
-    console.log('\nUsage:\n' + '  $ minkowski-sausage-cli <n>\n');
+    console.log('\nUsage:\n' + '  $ minkowski-sausage-cli <n>\n' + '  $ minkowski-sausage-cli <n> --inverse\n');
+}
+
+const getFlags = function(params) {
+    let flags = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (params[i].startsWith('-')) {
+                flags.push(params[i]);
+            }
+        }
+    }
+    return flags;
+}
+
+const getValues = function(params) {
+    let values = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (!params[i].startsWith('-')) {
+                values.push(params[i]);
+            }
+        }
+    }
+    return values;
+}
+
+const drawInverse = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && (flags[i].toLowerCase() === '-i' || flags[i].toLowerCase() === '--inverse')) {
+            return true;
+        }
+    }
+    return false;
 }
 
 if (process.argv.length > 2) {
     const params = process.argv.slice(2);
-    if (params[0] && !isNaN(params[0]) && parseInt(params[0]) >= 0) {
-        var n = parseInt(params[0]);
+    const values = getValues(params);
+    const flags = getFlags(params);
+    if (values[0] && !isNaN(values[0]) && parseInt(values[0]) >= 0) {
+        var n = parseInt(values[0]);
         if (n !== undefined) {
-            console.log(minkowski_sausage.create(n));
+            console.log(minkowski_sausage.create(n, drawInverse(flags)));
         }
     } else {
         console.log('\n<n> should be a number greater than or equal to 0');
--- src/index.js
+++ src/index.js
@@ -13,16 +13,6 @@ const createBoard = function(w, h) {
 const drawCurve = function(board, pos, type, params) {
   if (type === 'right') {
     board[pos.y][pos.x + 1] = '─';
-    board[pos.y][pos.x + 2] = '┐';
-    board[pos.y - 1][pos.x + 2] = '└';
-    board[pos.y - 1][pos.x + 3] = '─';
-    board[pos.y - 1][pos.x + 4] = '┘';
-    board[pos.y][pos.x + 4] = '│';
-    board[pos.y + 1][pos.x + 4] = '┌';
-    board[pos.y + 1][pos.x + 5] = '─';
-    board[pos.y + 1][pos.x + 6] = '┐';
-    board[pos.y][pos.x + 6] = '└';
-    board[pos.y][pos.x + 7] = '─';
 
     if (params && params.end1 === 'left') {
       board[pos.y][pos.x] = '─';
@@ -35,26 +25,16 @@ const drawCurve = function(board, pos, type, params) {
     }
 
     if (params && params.end2 === 'right') {
-      board[pos.y][pos.x + 8] = '─';
+      board[pos.y][pos.x + 2] = '─';
     }
     if (params && params.end2 === 'up') {
-      board[pos.y][pos.x + 8] = '┘';
+      board[pos.y][pos.x + 2] = '┘';
     }
     if (params && params.end2 === 'down') {
-      board[pos.y][pos.x + 8] = '┐';
+      board[pos.y][pos.x + 2] = '┐';
     }
   } else if (type === 'left') {
     board[pos.y][pos.x - 1] = '─';
-    board[pos.y][pos.x - 2] = '└';
-    board[pos.y + 1][pos.x - 2] = '┐';
-    board[pos.y + 1][pos.x - 3] = '─';
-    board[pos.y + 1][pos.x - 4] = '┌';
-    board[pos.y][pos.x - 4] = '│';
-    board[pos.y - 1][pos.x - 4] = '┘';
-    board[pos.y - 1][pos.x - 5] = '─';
-    board[pos.y - 1][pos.x - 6] = '└';
-    board[pos.y][pos.x - 6] = '┐';
-    board[pos.y][pos.x - 7] = '─';
 
     if (params && params.end1 === 'right') {
       board[pos.y][pos.x] = '─';
@@ -67,27 +47,15 @@ const drawCurve = function(board, pos, type, params) {
     }
 
     if (params && params.end2 === 'left') {
-      board[pos.y][pos.x - 8] = '─';
+      board[pos.y][pos.x - 2] = '─';
     }
     if (params && params.end2 === 'up') {
-      board[pos.y][pos.x - 8] = '└';
+      board[pos.y][pos.x - 2] = '└';
     }
     if (params && params.end2 === 'down') {
-      board[pos.y][pos.x - 8] = '┌';
+      board[pos.y][pos.x - 2] = '┌';
     }
   } else if (type === 'up') {
-    board[pos.y + 1][pos.x] = '┌';
-    board[pos.y + 1][pos.x + 1] = '─';
-    board[pos.y + 1][pos.x + 2] = '┘';
-    board[pos.y + 2][pos.x + 2] = '┐';
-    board[pos.y + 2][pos.x + 1] = '─';
-    board[pos.y + 2][pos.x] = '─';
-    board[pos.y + 2][pos.x - 1] = '─';
-    board[pos.y + 2][pos.x - 2] = '└';
-    board[pos.y + 3][pos.x - 2] = '┌';
-    board[pos.y + 3][pos.x - 1] = '─';
-    board[pos.y + 3][pos.x] = '┘';
-
     if (params && params.end1 === 'left') {
       board[pos.y][pos.x] = '┘';
     }
@@ -99,27 +67,15 @@ const drawCurve = function(board, pos, type, params) {
     }
 
     if (params && params.end2 === 'left') {
-      board[pos.y + 4][pos.x] = '┐';
+      board[pos.y + 1][pos.x] = '┐';
     }
     if (params && params.end2 === 'right') {
-      board[pos.y + 4][pos.x] = '┌';
+      board[pos.y + 1][pos.x] = '┌';
     }
     if (params && params.end2 === 'up') {
-      board[pos.y + 4][pos.x] = '│';
+      board[pos.y + 1][pos.x] = '│';
     }
   } else if (type === 'down') {
-    board[pos.y - 1][pos.x] = '┘';
-    board[pos.y - 1][pos.x - 1] = '─';
-    board[pos.y - 1][pos.x - 2] = '┌';
-    board[pos.y - 2][pos.x - 2] = '└';
-    board[pos.y - 2][pos.x - 1] = '─';
-    board[pos.y - 2][pos.x] = '─';
-    board[pos.y - 2][pos.x + 1] = '─';
-    board[pos.y - 2][pos.x + 2] = '┐';
-    board[pos.y - 3][pos.x + 2] = '┘';
-    board[pos.y - 3][pos.x + 1] = '─';
-    board[pos.y - 3][pos.x] = '┌';
-
     if (params && params.end1 === 'left') {
       board[pos.y][pos.x] = '┐';
     }
@@ -131,13 +87,13 @@ const drawCurve = function(board, pos, type, params) {
     }
 
     if (params && params.end2 === 'left') {
-      board[pos.y - 4][pos.x] = '┘';
+      board[pos.y - 1][pos.x] = '┘';
     }
     if (params && params.end2 === 'right') {
-      board[pos.y - 4][pos.x] = '└';
+      board[pos.y - 1][pos.x] = '└';
     }
     if (params && params.end2 === 'down') {
-      board[pos.y - 4][pos.x] = '│';
+      board[pos.y - 1][pos.x] = '│';
     }
   }
 }
@@ -153,13 +109,13 @@ const assignRelativePoints = function(queue) {
   for (let i = 0; i < queue.length; i++) {
     queue[i].pos = { x: currentPoint.x, y: currentPoint.y };
     if (queue[i].type === 'left') {
-      currentPoint = { x: currentPoint.x - 8, y: currentPoint.y };
+      currentPoint = { x: currentPoint.x - 2, y: currentPoint.y };
     } else if (queue[i].type === 'right') {
-      currentPoint = { x: currentPoint.x + 8, y: currentPoint.y };
+      currentPoint = { x: currentPoint.x + 2, y: currentPoint.y };
     } else if (queue[i].type === 'up') {
-      currentPoint = { x: currentPoint.x, y: currentPoint.y + 4 };
+      currentPoint = { x: currentPoint.x, y: currentPoint.y + 1 };
     } else if (queue[i].type === 'down') {
-      currentPoint = { x: currentPoint.x, y: currentPoint.y - 4 };
+      currentPoint = { x: currentPoint.x, y: currentPoint.y - 1 };
     }
   }
   return currentPoint;
@@ -200,69 +156,131 @@ const getDimensions = function(queue, finalPoint) {
     largestY = finalPoint.y;
   }
 
-  let bufferX = 0;
-  let bufferY = 0;
-  if (queue[0].type === 'left' || queue[0].type === 'right') {
-    bufferY = 1;
-  } else if (queue[0].type === 'up' || queue[0].type === 'down') {
-    bufferX = 2;
-  }
-
   return { 
-    pos: { x: Math.abs(lowestX) + bufferX, y: Math.abs(lowestY) + bufferY }, 
-    w: (largestX - lowestX) + (bufferX * 2) + 1, 
-    h: (largestY - lowestY) + (bufferY * 2) + 1
+    pos: { x: Math.abs(lowestX), y: Math.abs(lowestY) }, 
+    w: (largestX - lowestX) + 1, 
+    h: (largestY - lowestY) + 1
   };
 }
 
-const _minkowski = function(n, queue, type, params) {
-  if (n <= 1) {
-    // Add to queue
-    queue.push({ type: type, params: params });
-  } else {
-    if (type === 'right') {
-      _minkowski(n - 1, queue, 'right', { end1: params.end1, end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'left', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'up', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'left', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'down', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'down', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'left', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'up', end2: params.end2 });
-    } else if (type === 'down') {
-      _minkowski(n - 1, queue, 'down', { end1: params.end1, end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'up', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'right', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'up', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'left', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'left', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'up', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'right', end2: params.end2 });
-    } else if (type === 'up') {
-      _minkowski(n - 1, queue, 'up', { end1: params.end1, end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'down', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'left', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'down', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'right', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'right', end2: 'right' });
-      _minkowski(n - 1, queue, 'right', { end1: 'down', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'left', end2: params.end2 });
-    } else if (type === 'left') {
-      _minkowski(n - 1, queue, 'left', { end1: params.end1, end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'right', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'down', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'right', end2: 'down' });
-      _minkowski(n - 1, queue, 'down', { end1: 'up', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'up', end2: 'up' });
-      _minkowski(n - 1, queue, 'up', { end1: 'right', end2: 'left' });
-      _minkowski(n - 1, queue, 'left', { end1: 'down', end2: params.end2 });
+const getOppositeType = function(type) {
+  if (type === 'down') {
+    return 'up';
+  } else if (type === 'left') {
+    return 'right';
+  } else if (type === 'up') {
+    return 'down';
+  } else if (type === 'right') {
+    return 'left';
+  }
+  return 'none';
+}
+
+const addEndParams = function(queue, params) {
+  for (let i = 0; i < queue.length; i++) {
+    queue[i].params = {};
+
+    if (i === 0) {
+      queue[i].params.end1 = params && params.end1 ? params.end1 : getOppositeType(queue[i].type);
+    } else {
+      queue[i].params.end1 = getOppositeType(queue[i - 1].type);
+    }
+
+    if (i === queue.length - 1) {
+      queue[i].params.end2 = params && params.end2 ? params.end2 : queue[i].type;
+    } else {
+      queue[i].params.end2 = queue[i + 1].type;
     }
   }
 }
 
-const minkowski = function(n, type, params) {
+const _minkowski = function(n, queue, type, inverse=false) {
+  if (n <= 0) {
+    // Add to queue
+    queue.push({ type: type });
+  } else if (inverse) {
+    if (type === 'right') {
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+    } else if (type === 'down') {
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+    } else if (type === 'up') {
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+    } else if (type === 'left') {
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+    }
+  } else {
+    if (type === 'right') {
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+    } else if (type === 'down') {
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'down');
+    } else if (type === 'up') {
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'right');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+    } else if (type === 'left') {
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'up');
+      _minkowski(n - 1, queue, 'left');
+      _minkowski(n - 1, queue, 'down');
+      _minkowski(n - 1, queue, 'left');
+    }
+  }
+}
+
+const minkowski = function(n, type, params, inverse=false) {
   let queue = [];
-  _minkowski(n, queue, type, params);
+  _minkowski(n, queue, type, inverse);
+  addEndParams(queue, params);
 
   const finalPoint = assignRelativePoints(queue);
   const dimensions = getDimensions(queue, finalPoint);
@@ -288,20 +306,12 @@ const draw = function(board) {
   return result;
 }
 
-const create = function(n) {
+const create = function(n, inverse=false) {
   if (n === undefined || isNaN(n) || n < 0) {
     return '';
   }
 
-  if (n === 0) {
-    const board = createBoard(3, 1);
-    board[0][0] = '─';
-    board[0][1] = '─';
-    board[0][2] = '─';
-    return draw(board);
-  }
-
-  const board = minkowski(n, 'right', { end1: 'left', end2: 'right' });
+  const board = minkowski(n, 'right', { end1: 'left', end2: 'right' }, inverse);
   
   return draw(board);
 }
